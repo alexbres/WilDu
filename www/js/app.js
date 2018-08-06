a045c3b0d71a3769f4668c547063dd035494e8b6
Алексей <alexbres@gmail.com>
2018-08-06 12:21:02 -0700

    load routes through requirejs

--- www/js/app.js
+++ www/js/app.js
@@ -1,51 +1,53 @@
-var $$ = Dom7;
-var app = new Framework7({
-  // App root element
-  root: '#app',
-  // App Name
-  name: 'WilDu',
-  // App id
-  id: 'com.wildu.app',
-  // Enable swipe panel
-  panel: {
-    swipe: 'left',
-  },
-  routes: routes,
-  // ... other parameters
-});
-
-var mainView = app.views.create('.view-main');
-
-$$(document).on('page:init', '.page[data-name="worksetEdit"]', function (e) {
-  console.log('worksetEdit init');
-  initWorksetEdit();
-})
-
-var workItems = [];
-
-function initWorksetEdit() {
-  var addWorkItem = new Vue({
-    el: '#addWorkItem',
-    data: {
-      name: '123'
+define(['routes'], function (routes) {
+  var $$ = Dom7;
+  var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'WilDu',
+    // App id
+    id: 'com.wildu.app',
+    // Enable swipe panel
+    panel: {
+      swipe: 'left',
     },
-    methods: {
-      add: function () {
-        console.log(this.name);
-        workItems.push(this.name);
-        workItems.forEach(function(w) {
-          console.log(w);
-        })
+    routes: routes,
+    // ... other parameters
+  });
+
+  var mainView = app.views.create('.view-main');
+
+  $$(document).on('page:init', '.page[data-name="worksetEdit"]', function (e) {
+    console.log('worksetEdit init');
+    initWorksetEdit();
+  })
+
+  var workItems = [];
+
+  function initWorksetEdit() {
+    var addWorkItem = new Vue({
+      el: '#addWorkItem',
+      data: {
+        name: '123'
+      },
+      methods: {
+        add: function () {
+          console.log(this.name);
+          workItems.push(this.name);
+          workItems.forEach(function (w) {
+            console.log(w);
+          })
+        }
       }
-    }
-  });
+    });
 
-  var workItemList = new Vue({
-    el: '#workItemList',
-    data: {
-      items: workItems
-    },
-    methods: {
-    }
-  });
-}
+    var workItemList = new Vue({
+      el: '#workItemList',
+      data: {
+        items: workItems
+      },
+      methods: {
+      }
+    });
+  }
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,17 +1,19 @@
-var routes = [
-  {
-    path: '/',
-    url: './index.html',
-    name: 'home'
-  }, 
-  {
-    path: '/about/',
-    url: './pages/about.html',
-    name: 'about'
-  }, 
-  {
-    path: '/worksetEdit/',
-    url: './pages/worksetEdit.html',
-    name: 'worksetEdit'
-  }
-]
+define(function () {
+  return [
+    {
+      path: '/',
+      url: './index.html',
+      name: 'home'
+    },
+    {
+      path: '/about/',
+      url: './pages/about.html',
+      name: 'about'
+    },
+    {
+      path: '/worksetEdit/',
+      url: './pages/worksetEdit.html',
+      name: 'worksetEdit'
+    }
+  ]
+});
