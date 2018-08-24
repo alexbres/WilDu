07fd2dc92fa79881053bc746b9576f653a533d33
Алексей <alexbres@gmail.com>
2018-08-24 14:32:08 -0700

    impl saving new task in DB

---
+++ www/js/database.js
@@ -0,0 +1,52 @@
+define(()=>{
+
+  window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+  window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+  if (!window.indexedDB) {
+    window.alert('Your browser don\'t support a stable version of IndexedDB.');
+  }
+
+  const dbName = "Task";
+
+  var request = indexedDB.open(dbName);
+  var db;
+
+  request.onerror = function(event) {
+    alert("Why didn't you allou to use IndexedDB?");
+  };
+
+  request.onsuccess = function(event) {
+    db = event.target.result;
+  };
+
+  request.onupgradeneeded = function(event) {
+    var db = event.target.result;
+  
+    var objectStore = db.createObjectStore("task", { keyPath: "id", autoIncrement:true });
+  
+    objectStore.createIndex("name", "name", { unique: false });
+    };
+
+  function addTask(task) {
+    var transaction = db.transaction(["task"], "readwrite");
+
+    transaction.oncomplete = function (event) {
+      console.log("transaction done!");
+    };
+  
+    transaction.onerror = function (event) {
+      console.log('transaction error');
+    };
+  
+    var objectStore = transaction.objectStore("task");
+    var request = objectStore.add(task);
+    request.onsuccess = function (event) {
+      console.log('data was added');
+    }
+  }
+  return {
+    addTask: addTask,
+  }
+});
